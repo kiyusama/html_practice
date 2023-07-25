bc782e4140bd34a778b1c8c485679a439464a31f
kiyusama <140500955+kiyusama@users.noreply.github.com>
2023-07-25 18:47:08 +0900

    Initial commit
    練習です。

---
+++ login.js
@@ -0,0 +1,19 @@
+"use strict";
+let userName
+let Password
+
+userName = prompt("what's your name?","");
+if (userName === "Admin"){// ===とする点に注意
+Password = prompt("Pls fill in your password.","");
+   if (Password === "abc"){
+    alert("welcome");
+   } else if (Password === null || Password === ""){// キャンセルを押した場合と何も入力しなかった場合
+    alert("canceled");
+   } else {
+    alert("Password is wrong");
+   }
+} else if (userName === null || userName === ""){
+    alert("canceled");
+} else {
+    alert("I don't know you");
+}
