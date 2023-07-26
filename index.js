91d05f5d35cb08ec79e4da8b3f27daf8ea1bb75c
kiyusama <140500955+kiyusama@users.noreply.github.com>
2023-07-26 20:13:00 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";
+let userName
+let Password
+
+userName = prompt("what's your name?","");
+if (userName === "masa"){// ===とする点に注意
+Password = prompt("Pls fill in your password.","");
+   if (Password === "abc"){
+    alert("welcome");
+    window.location.href = "top.html";
+   } else if (Password === null || Password === ""){// キャンセルを押した場合と何も入力しなかった場合
+    alert("canceled");
+   } else {
+    alert("Password is wrong");
+   }
+} else if (userName === null || userName === ""){
+    alert("canceled");
+} else {
+    alert("I don't know you");
+}
